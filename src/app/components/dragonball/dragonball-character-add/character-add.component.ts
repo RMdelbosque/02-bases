f981330f70cb6b6932302b608500351f023cda8e
Delbosque <rodrigo.m.delbosque@gmail.com>
2025-08-27 20:06:20 +0200

    Change addCharacter-DragonBall

---
+++ src/app/components/dragonball/dragonball-character-add/character-add.component.ts
@@ -0,0 +1,33 @@
+import { Component, input, signal } from '@angular/core';
+import type { Character } from '../../../interfaces/character.interface';
+
+@Component({
+  selector: 'dragonball-character-add',
+  templateUrl: './character-add.component.html',
+})
+export class CharacterAddComponent {
+
+  name = signal('');
+  power = signal(0);
+
+  addCharacter() {
+    if (!this.name() || !this.power() || this.power() <= 0 ){
+      return;
+    }
+
+    const newCharacter: Character = {
+      id: 1000,
+      name: this.name(),
+      power: this.power(),
+    };
+
+    // this.characters.update((list) => [... list, newCharacter]);
+    console.log({ newCharacter});
+    this.resetFields();
+  }
+
+  resetFields() {
+    this.name.set('');
+    this.power.set(0);
+  }
+}
--- src/app/pages/dragonball-super/dragonball-super-page.component.ts
+++ src/app/pages/dragonball-super/dragonball-super-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, signal } from '@angular/core';
+import { CharacterListComponent } from "../../components/dragonball/character-list/character-list.component";
+import { CharacterAddComponent } from "../../components/dragonball/dragonball-character-add/character-add.component";
 
 interface Character {
   id: number;
@@ -8,14 +10,14 @@ interface Character {
 
 
 @Component({
-  imports: [],
+  imports: [CharacterListComponent, CharacterAddComponent],
   templateUrl: './dragonball-super-page.component.html'
 })
 export class DragonballSuperPageComponent {
 
-  name = signal('Gohan');
-  power = signal(100);
-  
+  name = signal('');
+  power = signal(0);
+
   characters = signal<Character[]>([
     {id: 1, name: 'Goku', power: 9001},
     {id: 2, name: 'Vegeta', power: 8000},
@@ -27,12 +29,13 @@ export class DragonballSuperPageComponent {
     }
 
     const newCharacter: Character = {
-      id: this.characters().length +1,
+      id: 1000,
       name: this.name(),
       power: this.power(),
     };
 
-    this.characters.update((list) => [... list, newCharacter]);
+    // this.characters.update((list) => [... list, newCharacter]);
+    console.log({ newCharacter});
     this.resetFields();
   }
 
