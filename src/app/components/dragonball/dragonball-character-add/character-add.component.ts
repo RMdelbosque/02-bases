507604d7b1db0599e0f26a7dba602960578b8253
Delbosque <rodrigo.m.delbosque@gmail.com>
2025-08-29 15:21:55 +0200

    Finalizada sesiuón 5

--- src/app/components/dragonball/dragonball-character-add/character-add.component.ts
+++ src/app/components/dragonball/dragonball-character-add/character-add.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, signal } from '@angular/core';
+import { Component, input, output, signal } from '@angular/core';
 import type { Character } from '../../../interfaces/character.interface';
 
 @Component({
@@ -9,6 +9,8 @@ export class CharacterAddComponent {
 
   name = signal('');
   power = signal(0);
+
+  newCharacter = output<Character>();
 
   addCharacter() {
     if (!this.name() || !this.power() || this.power() <= 0 ){
@@ -22,7 +24,7 @@ export class CharacterAddComponent {
     };
 
     // this.characters.update((list) => [... list, newCharacter]);
-    console.log({ newCharacter});
+    this.newCharacter.emit(newCharacter);
     this.resetFields();
   }
 
--- src/app/pages/dragonball-super/dragonball-super-page.component.ts
+++ src/app/pages/dragonball-super/dragonball-super-page.component.ts
@@ -1,12 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CharacterListComponent } from "../../components/dragonball/character-list/character-list.component";
 import { CharacterAddComponent } from "../../components/dragonball/dragonball-character-add/character-add.component";
-
-interface Character {
-  id: number;
-  name: string;
-  power: number;
-}
+import { DragonballService } from '../../services/dragonball.service';
 
 
 @Component({
@@ -14,33 +9,5 @@ interface Character {
   templateUrl: './dragonball-super-page.component.html'
 })
 export class DragonballSuperPageComponent {
-
-  name = signal('');
-  power = signal(0);
-
-  characters = signal<Character[]>([
-    {id: 1, name: 'Goku', power: 9001},
-    {id: 2, name: 'Vegeta', power: 8000},
-  ]);
-
-  addCharacter() {
-    if (!this.name() || !this.power() || this.power() <= 0 ){
-      return;
-    }
-
-    const newCharacter: Character = {
-      id: 1000,
-      name: this.name(),
-      power: this.power(),
-    };
-
-    // this.characters.update((list) => [... list, newCharacter]);
-    console.log({ newCharacter});
-    this.resetFields();
-  }
-
-  resetFields() {
-    this.name.set('');
-    this.power.set(0);
-  }
+  public dragonballService = inject(DragonballService);
 }
